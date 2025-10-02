46d660a8e9b762c6ba01e545cf6159ee0c630d34
Zhaneta-Lecini <zhaneta.lecini@student.nbi-handelsakademin.se>
2025-10-02 09:59:05 +0200

    //jag vill att användaren ska hopa i menun och hanterar alla case

--- Program.cs
+++ Program.cs
@@ -1,66 +1,85 @@
-﻿using App;
+﻿using System.ComponentModel;
+using App;
 
-//för att läga till i listan.//Skapar ny (objekt) User som heter "MyUser" och har string värden "Anna"..
-
+//user lista,(har vält en lista för att det är inte med en fixed size, som t.ex array)//för att läga till i listan.
+//Skapar en lista av användare (users). Här sparar jag alla User-objekt som finns i systemet.
 List<User> users = new List<User>();
-User MyUser = new User("Anna", "J456n");//"Skapa ny användare"
-User MyUser2 = new User("Inna", "123456");
-users.Add(MyUser);
+User MyUser = new User("Anna@hotmail.com", "J456n"); //Här skapar jag två användare (MyUser och MyUser2) med e-post och lösenord. 
+User MyUser2 = new User("Inna@hotmail.com", "123456");
+users.Add(MyUser); //Sedan lägger jag till dem i listan users.
 users.Add(MyUser2);
         
 
-      //   Skapar ny (objekt) User som heter "MyUser" och har string värden "Anna"..items.Add(MyItem);
+//   Skapar en lista med items (name, description, owner)
 List <Item> items = new List<Item>();
 items.Add(new Item("Stol", "Black", MyUser)); //items.Add(new Item("name/skor", "black/descrip", owner));
 items.Add(new Item("Bord", "White", MyUser2));
 
 
 
-User? active_user = null;
+User? active_user = null; // jag har skapat en variabel active_user som håller koll på vem som är inloggad.
+                        //null betyder att ingen användare är inloggad ännu.
 
-bool running = true;
+bool running = true; //Så länge running är true kommer while-loopen att fortsätta köra programmet.
 while (running)
 {
-    if (active_user == null) //användaren är inte aktiv
+    if (active_user == null) //användaren är inte aktiv, då visas menun med 6 val
     {
-        Console.WriteLine("Welcome to trading market");
-        Console.WriteLine("menu");
-        Console.WriteLine("sök");
-        Console.WriteLine("1.skapa konto");
-        Console.WriteLine("2.logga in");
-        Console.WriteLine("Enter 'q' to exit");
+        Console.WriteLine("1.Welcome to trading market");
+        Console.WriteLine("2.Menu");
+        Console.WriteLine("3.sök");
+        Console.WriteLine("4.skapa konto");
+        Console.WriteLine("5.logga in");
+        Console.WriteLine("6.Enter 'q' to exit");
 
         string input = Console.ReadLine();
 
-        switch (input)
+        switch (input) //jag vill att användaren ska hopa i menun och hanterar alla case 
         {
-            case "2":
+            case "4"://skapa ny konto
+                Console.Write("Ange username: ");
+                string newEmail = Console.ReadLine();
+
+                Console.Write("Ange password: ");
+                string newPassword = Console.ReadLine();
+
+                users.Add(new User(newEmail, newPassword));//Sedan skapas en ny användare och läggs till i listan (users.Add(...)).
+                Console.WriteLine("Konto skapat!");
+                break;
+
+
+            case "5": //loga in
                 Console.Write("Please enter your email: ");
                 string userName = Console.ReadLine();
+
                 Console.Write("Please enter your password: ");
                 string userPassword = Console.ReadLine();
 
-                users.Add(new User(userName, userPassword));
+                
 
-                foreach (User user in users)
+                foreach (User user in users) //vi kollar om användaren redan finns
                 {
                     if (user.UserName == userName && user.Password == userPassword)
                     {
                         active_user = user;
-                        Console.WriteLine("Successfull Login!");
+                        Console.WriteLine("Successfull Login!"); //if användnamn och pass matchar med de som vi har i User class då han har access annash visav menun igen, med 6 val
                     }
                 }
+                break;
+
+            case "6":
+                running = false;  // avslutas.
 
                 break;
 
-            case "q":
-                running = false;
+                default://om ingen stämer
+                Console.WriteLine("Fel, försök igen");
                 break;
         }
 
     }
 
-    if (active_user != null)
+    if (active_user != null) //if användare är inte null:är inloggat, visas en ny meny för inloggade användare.
     {
         Console.WriteLine("Welcome to trading market");
         Console.WriteLine("menu");
